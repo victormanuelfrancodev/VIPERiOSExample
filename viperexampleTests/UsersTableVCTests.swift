7bc8ae8fd5a840adbe7fef35609edb6ba0d5f417
Victor Manuel Lagunas Franco <victormanuelfrancodev@gmail.com>
2024-04-25 18:02:13 +0900

    format

--- viperexampleTests/UsersTableVCTests.swift
+++ viperexampleTests/UsersTableVCTests.swift
@@ -15,19 +15,19 @@ import UIKit
 class UsersTableVCTests: XCTestCase {
     
     var viewController:UserViewController!
-        
+    
     override func setUp(){
         super.setUp()
         
         viewController = UserViewController()
         viewController.loadViewIfNeeded() //
-    
+        
     }
     
     func test_TableIsHiddenWhenUsersIsEmpty() {
         let expectation = XCTestExpectation(description: "Table doesnt have users, then table is hidden")
         DispatchQueue.main.async {
-
+            
             XCTAssertTrue(self.viewController.isTableViewHidden,"The table is empty, then hidden.")
             expectation.fulfill()
         }
@@ -42,7 +42,7 @@ class UsersTableVCTests: XCTestCase {
         self.viewController.update(with: users)
         
         DispatchQueue.main.async {
-        
+            
             XCTAssertFalse(self.viewController.isTableViewHidden,"The table fetch users then not hidden.")
             expectation.fulfill()
         }
@@ -54,7 +54,7 @@ class UsersTableVCTests: XCTestCase {
     func test_LabelIsHiddenWhenStartApplication(){
         let expectation = XCTestExpectation(description: "User Label is not hidden when start the application")
         DispatchQueue.main.async {
-
+            
             XCTAssertTrue(self.viewController.isLableViewHidden,"The label is hidden.")
             expectation.fulfill()
         }
@@ -66,7 +66,7 @@ class UsersTableVCTests: XCTestCase {
         self.viewController.update(with: "problema")
         let expectation = XCTestExpectation(description: "User Label is not hidden when table doesnt have data or has a problem")
         DispatchQueue.main.async {
-
+            
             XCTAssertFalse(self.viewController.isLableViewHidden,"The label not is hidden.")
             expectation.fulfill()
         }
@@ -78,7 +78,7 @@ class UsersTableVCTests: XCTestCase {
         self.viewController.update(with: [User(name: "Enoc")])
         let expectation = XCTestExpectation(description: "User Label is hidden when table has data")
         DispatchQueue.main.async {
-
+            
             XCTAssertTrue(self.viewController.isLableViewHidden,"The label is hidden.")
             expectation.fulfill()
         }
